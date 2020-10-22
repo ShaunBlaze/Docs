51c9883684c0c54ea5399e3a259a376d42a0c5dd
Anvesha Rajan <44029156+anvy2803@users.noreply.github.com>
2020-10-22 20:19:45 +0530

    Update Count of substrings with atleast k distinct characters.cpp

--- Count of substrings with atleast k distinct characters.cpp
+++ Count of substrings with atleast k distinct characters.cpp
@@ -2,24 +2,6 @@
 using namespace std;
 typedef long long ll;
 
-/*vector<int> v;
-int dp[100][100];
-int dfs(int s, int prev)
-{
-  if(s==v.size())
-    return 0;
-  if(prev!=-1 && dp[s][prev]!=-1)
-    return dp[s][prev];
-  int ans=0;
-  for(int i=s;i<v.size();i++)
-  {
-    if(prev==-1 || v[i]%prev==0)
-      ans=max(ans,v[i]+dfs(i+1,v[i]));
-  }
-  if(prev!=-1)
-    dp[s][prev]=ans;
-  return ans;
-}*/
 int solve(int k, string s)
 {
   int i,ans=0,j=0;
